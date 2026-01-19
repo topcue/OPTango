a086bfa6fcb2450ff4c7cd4ff3420a5269870a05
topcue <topcue1019@gmail.com>
2026-01-19 05:39:40 +0000

    Cleanup

--- env/setup_conda_env.sh
+++ env/setup_conda_env.sh
@@ -6,7 +6,6 @@ conda tos accept
 
 conda create -n optango python=3.12 -y
 conda activate optango
-# conda install pandas tqdm -y
 
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cpu
 
