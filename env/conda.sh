7cc745cf20e7ecb6131f2f723c28a54d0eace89b
topcue <topcue1019@gmail.com>
2026-01-19 06:49:36 +0000

    Fix conda.sh

--- env/conda.sh
+++ env/conda.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
+SCRIPT_PATH="$(readlink -f "$0")"       # OPTango/env/conda.sh
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"  # OPTango/env
+CONDA_DIR="${SCRIPT_DIR}/miniconda3"
+
+source ${CONDA_DIR}/bin/activate
+
 conda activate optango
 
 # EOF
