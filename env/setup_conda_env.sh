33e5007ba95c7502789e7fee2af5161d54e2a707
topcue <topcue1019@gmail.com>
2026-01-19 06:18:51 +0000

    Intergrate pip installations to setup script

--- env/setup_conda_env.sh
+++ env/setup_conda_env.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # OPTango/env
 CONDA_DIR="${SCRIPT_DIR}/miniconda3"
+OPTANGO_DIR="$(cd "${SCRIPT_DIR}/.." && pwd -P)"
 
 source ${CONDA_DIR}/bin/activate
 
@@ -11,5 +12,7 @@ conda create -n optango python=3.12 -y
 conda activate optango
 
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+
+pip3 install -r ${OPTANGO_DIR}/requirements.txt
 
 # EOF
