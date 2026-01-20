f742a6506221b08c3bf274d644364f235aef2357
topcue <topcue1019@gmail.com>
2026-01-20 12:10:40 +0900

    Fix formatting

--- third/jTrans/datautils/process.py
+++ third/jTrans/datautils/process.py
@@ -1,26 +1,60 @@
+import sys
+
+sys.path.insert(0, r"C:\Users\user\workspace\IDA-python\optango\Lib")
+sys.path.insert(0, r"C:\Users\user\workspace\IDA-python\optango\libs")
+sys.path.insert(0, r"C:\Users\user\workspace\IDA-python\optango")
+sys.path.insert(0, r"C:\Users\user\workspace\IDA-python\optango\Lib\site-packages")
+
+#! =============================================================================
+
+import os
+from collections import defaultdict
+
 import idc
 import idautils
 import idaapi
 import pickle
 import binaryai
 import networkx as nx
-import sys, os
-sys.path.insert(0, os.path.split(__file__)[0])  # in case ModuleNotFoundError: No module named 'util'
 from util.base import Binarybase
-from path_config import SAVE_ROOT, data_path, strip_path
-from util.path_utils import mkdir_of_file, get_save_path
+
+WSL_PREFIX = "/home/user/win_workspace"
+WIN_PREFIX = "C:/Users/user/workspace"
+
+def wsl_to_win_path(p):
+    if p == WSL_PREFIX or p.startswith(WSL_PREFIX + "/"):
+        return WIN_PREFIX + p[len(WSL_PREFIX):]
+    return p
+
+def win_to_wsl_path(p):
+    if p == WIN_PREFIX or p.startswith(WIN_PREFIX + "/"):
+        return WSL_PREFIX + p[len(WIN_PREFIX):]
+    return p
+
+# SAVEROOT = "./extract" # dir of pickle files saved by IDA
+# DATAROOT = "./dataset" # dir of binaries (not stripped)
+
+BASE_PATH = "/home/user/win_workspace/storage/optango"
+
+DATA_ROOT = os.path.join(BASE_PATH, "dataset")
+DATA_ROOT_WIN = wsl_to_win_path(DATA_ROOT)
+
+SAVE_ROOT = os.path.join(BASE_PATH, "extract")
+SAVE_ROOT_WIN = wsl_to_win_path(SAVE_ROOT)
 
 
 class BinaryData(Binarybase):
     def __init__(self, unstrip_path):
         super(BinaryData, self).__init__(unstrip_path)
         self.fix_up()
-    
+
+
     def fix_up(self):
         for addr in self.addr2name:
             # incase some functions' instructions are not recognized by IDA
             idc.create_insn(addr)  
             idc.add_func(addr) 
+
 
     def get_asm(self, func):
         instGenerator = idautils.FuncItems(func)
@@ -29,12 +63,14 @@ class BinaryData(Binarybase):
             asm_list.append(idc.GetDisasm(inst))
         return asm_list
 
+
     def get_rawbytes(self, func):
         instGenerator = idautils.FuncItems(func)
         rawbytes_list = b""
         for inst in instGenerator:
             rawbytes_list += idc.get_bytes(inst, idc.get_item_size(inst))
         return rawbytes_list
+
 
     def get_cfg(self, func):
 
@@ -70,8 +106,10 @@ class BinaryData(Binarybase):
                 nx_graph.add_edge(block.start_ea, succ.start_ea)
         return nx_graph  
 
+
     def get_binai_feature(self, func):
         return binaryai.ida.get_func_feature(func)
+
 
     def extract_all(self):
         for func in idautils.Functions():
@@ -85,37 +123,38 @@ class BinaryData(Binarybase):
             yield (self.addr2name[func], func, asm_list, rawbytes_list, cfg, bai_feature)
 
 
-if __name__ == "__main__":
-    import os
-    from collections import defaultdict
-    os.makedirs(SAVE_ROOT, exist_ok=True)
-    assert os.path.exists(SAVE_ROOT), f"SAVE_ROOT {SAVE_ROOT} does not exist"
+def main():
+    DEBUG = True
 
-    # use strip file for ida, and use unstripped file for elf to obtain funcname and addr
+    assert os.path.exists(DATA_ROOT_WIN), "DATA_ROOT does not exist"
+    assert os.path.exists(SAVE_ROOT_WIN), "SAVE_ROOT does not exist"
+
     binary_abs_path = idc.get_input_file_path()
+    if DEBUG:
+        print(f"[DEBUG] binary_abs_path: {binary_abs_path}")
 
-    assert binary_abs_path.endswith(".strip") and binary_abs_path.startswith(strip_path), binary_abs_path
-    strip_bin_dir, filename = os.path.split(binary_abs_path)
-    bin_dir_suffix, filename = strip_bin_dir[len(strip_path):], filename[:-6]   # remove .strip
-    unstrip_path = os.path.join(data_path + bin_dir_suffix, filename)
-    saved_path = get_save_path(SAVE_ROOT, bin_dir_suffix, filename)  # unpair data
-    print("[jTrans.process] data path:      ", unstrip_path)
-    print("[jTrans.process] strip data path:", binary_abs_path)
-    print("[jTrans.process] save path:      ", saved_path)
-    # with open("test.log", 'w') as f:
-    #     print(filename, file=f)
-    #     print(binary_abs_path, file=f)
-    #     print(DATAROOT, file=f)
-    #     print(unstrip_path, file=f)
+    filename = binary_abs_path.split('\\')[-1].replace(".strip", '')
+    unstrip_path = os.path.join(DATA_ROOT_WIN, filename)
+
+    if DEBUG:
+        print(f"[DEBUG] DATA_ROOT_WIN: {DATA_ROOT_WIN}")
+        print(f"[DEBUG] DATA_ROOT: {DATA_ROOT}")
+        print(f"[DEBUG] filename: {filename}")
+        print(f"[DEBUG] unstrip_path: {unstrip_path}")
+    
     idc.auto_wait()
     binary_data = BinaryData(unstrip_path)
 
     saved_dict = defaultdict(lambda: list)
-    mkdir_of_file(saved_path)
+    saved_path = os.path.join(SAVE_ROOT_WIN, filename + "_extract.pkl") # unpair data
     with open(saved_path, 'wb') as f:
         for func_name, func, asm_list, rawbytes_list, cfg, bai_feature in binary_data.extract_all():
             saved_dict[func_name] = [func, asm_list, rawbytes_list, cfg, bai_feature]
-            print("[jTrans.process]", func_name)
         pickle.dump(dict(saved_dict), f)
-    print("[jTrans.process]", "save result into", saved_path)
-    idc.qexit(0)  # exit IDA
+    idc.qexit(0) # exit IDA
+
+
+if __name__ == "__main__":
+    main()
+
+# EOF
--- third/jTrans/datautils/run.py
+++ third/jTrans/datautils/run.py
@@ -1,95 +1,85 @@
 import os
 import pickle
 import shutil
-import subprocess
 import multiprocessing
 import time
 from util.pairdata import pairdata
 from util.path_utils import mkdir_of_file, get_save_path, get_ida_log_path
 import argparse
+from tqdm import tqdm
 
+from my_config import wsl_to_win_path, run_ida
 
-def write_path_to_path_config_script():
-    cur_dir = os.path.split(__file__)[0]
-    f = open(os.path.join(cur_dir, "path_config.py"), 'w')
-    f.write("# this file is auto-generate by run.py, modify run.py if you need change the path in this file.\n")
-    for path_name in ["data_path", 'strip_path', 'SAVE_ROOT']:
-        path = os.path.abspath(eval(path_name)).replace('\\', '\\\\')
-        f.write(f"{path_name} = '{path}'\n")
-    f.close()
+from my_config import (
+    IDA_PATH,
+    BASE_PATH,
+    STRIP_PATH,
+    IDA_SCRIPT_PATH,
+    DATA_ROOT,
+    SAVE_ROOT,
+    NUM_JOBS,
+    LOG_PATH,
+    IDB_PATH,
+)
 
-
-class BOBUtil(object):
-    _WALK_RETURN = {
-        "bob": lambda data_dir, bench, opt, binary: (bench, opt, binary),
-        "path": lambda data_dir, bench, opt, binary: os.path.join(data_dir, bench, opt, binary),
-        "path+bob": lambda data_dir, bench, opt, binary:
-            (os.path.join(data_dir, bench, opt, binary), bench, opt, binary),
-    }
-
-    @staticmethod
-    def walk_bob_dir(data_dir, bench_list=None, opt_list=None, return_mode='bob'):
-        return_func = BOBUtil._WALK_RETURN[return_mode]
-        for bench in os.listdir(data_dir):
-            if bench_list is not None and bench not in bench_list:
-                continue
-            bench_dir = os.path.join(data_dir, bench)
-            if os.path.isfile(bench_dir):
-                continue
-
-            for opt in os.listdir(bench_dir):
-                if opt_list is not None and opt not in opt_list:
-                    continue
-                opt_dir = os.path.join(bench_dir, opt)
-                if os.path.isfile(opt_dir):
-                    continue
-
-                for binary in os.listdir(opt_dir):
-                    yield return_func(data_dir, bench, opt, binary)
-
-
-def normalize_path(path):
-    abs_path = os.path.abspath(path)
-    return os.path.relpath(abs_path, os.getcwd())
-
-
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data-path", default="./dataset/example")
-    parser.add_argument("--data-name", default="example")
-    parser.add_argument("--ida-path", default="ida/idat64.exe")
     parser.add_argument("--cpu-rate", type=float, default=0.5)
     args = parser.parse_args()
 
-    ida_path = args.ida_path
-    data_path, data_name = args.data_path, args.data_name
-    strip_path = f"dataset_strip/{data_name}"
-    script_path = "process.py"
-    SAVE_ROOT = f"extract/{data_name}"  # dir of pickle files saved by IDA
-    ida_log_dir = f"log/{data_name}"
-    ida_idb_dir = f"idb/{data_name}"
+    ida_log_dir = LOG_PATH
+    ida_idb_dir = IDB_PATH
 
-    data_path = normalize_path(data_path)
-    write_path_to_path_config_script()
+    print(f"[*] IDA_PATH:     {IDA_PATH}")
+    print(f"[*] BASE_PATH:    {BASE_PATH}")
+    print(f"[*] BASE_PATH:    {BASE_PATH}")
+    print(f"[*] DATA_ROOT:    {DATA_ROOT}")
+    print(f"[*] STRIP_PATH:   {STRIP_PATH}")
+    print(f"[*] SAVE_ROOT:    {SAVE_ROOT}")
+    print(f"[*] LOG_PATH:     {LOG_PATH}")
+    print(f"[*] IDB_PATH:     {IDB_PATH}")
+    print()
+
+    os.makedirs(STRIP_PATH, exist_ok=True)
+    os.makedirs(IDB_PATH,   exist_ok=True)
+    os.makedirs(LOG_PATH,   exist_ok=True)
+    os.makedirs(SAVE_ROOT, exist_ok=True)
 
     start = time.time()
-    # bin_pathes = sorted([bin_path for bin_path in BOBUtil.walk_bob_dir(data_path, return_mode='path')])
-    bin_pathes = sorted([os.path.join(root, file) for root, dirs, files in os.walk(data_path) for file in files])
+    bin_pathes = sorted([os.path.join(root, file) for root, dirs, files in os.walk(DATA_ROOT) for file in files])
 
-    pool = multiprocessing.Pool(processes=int(multiprocessing.cpu_count() * args.cpu_rate))
+    #! TODO: Fix me
+    bin_pathes = bin_pathes[:2]
+
+    jobs = []
+    # pool = multiprocessing.Pool(processes=int(multiprocessing.cpu_count() * args.cpu_rate))
     for bin_path in bin_pathes:
+        print(f"[*] bin_path: {bin_path}")
+
         bin_dir, bin_name = os.path.split(bin_path)
-        assert bin_dir.startswith(data_path), (bin_dir, data_path)
-        bin_dir_suffix = bin_dir[len(data_path):]
-        strip_bin_path = os.path.join(strip_path + bin_dir_suffix, f"{bin_name}.strip")
+        print(f"[*] bin_dir:  {bin_dir}")
+        print(f"[*] bin_name: {bin_name}")
+        assert bin_dir.startswith(DATA_ROOT), (bin_dir, DATA_ROOT)
+
+        # bin_dir_suffix = bin_dir[len(DATA_ROOT):]
+        bin_dir_suffix = ""
+
+        strip_bin_path = os.path.join(STRIP_PATH + bin_dir_suffix, f"{bin_name}.strip")
+        print(f"[*] strip_bin_path: {strip_bin_path}")
+        
+
         if not os.path.exists(strip_bin_path):
+            print(os.path.split(strip_bin_path)[0])
             mkdir_of_file(strip_bin_path)
             cmd = f"strip -s {bin_path} -o {strip_bin_path}"
             print("[jTrans.run]", cmd)
             os.system(cmd)
+
+            #! ???
             if not os.path.exists(strip_bin_path):  # if not x86 elf
                 os.makedirs(os.path.split(strip_bin_path)[0], exist_ok=True)
                 shutil.copy(bin_path, strip_bin_path)
+
 
         save_path = get_save_path(SAVE_ROOT, bin_dir_suffix, bin_name)
         if os.path.exists(save_path):
@@ -98,29 +88,59 @@ if __name__ == '__main__':
                 continue
             except EOFError as e:  # wrong pkl file
                 pass
+
         print("[jTrans.run] to obtain", save_path)
 
         log_path = get_ida_log_path(ida_log_dir, bin_dir_suffix, bin_name)
         idb_path = os.path.join(ida_idb_dir + bin_dir_suffix, f'{bin_name}.idb')
         mkdir_of_file(log_path)
         mkdir_of_file(idb_path)
-        cmd = [ida_path, f'-L{log_path}', '-c', '-A', f'-S{script_path}', f'-o{idb_path}', f'{strip_bin_path}']
-        print(" ".join(cmd))
-        pool.apply_async(subprocess.call, args=(cmd,))
-    pool.close()
-    pool.join()
+
+
+        log_path_win = wsl_to_win_path(log_path)
+        idb_path_win = wsl_to_win_path(idb_path)
+        strip_bin_path_win = wsl_to_win_path(strip_bin_path)
+
+        cmd_win = [IDA_PATH, f'-L{log_path_win}', '-c', '-A', f'-S{IDA_SCRIPT_PATH}', f'-o{idb_path_win}', f'{strip_bin_path_win}']
+        print(" ".join(cmd_win))
+
+        out_path = os.path.join(f"{log_path}.ida.stdout.txt")
+        err_path = os.path.join(f"{log_path}.ida.stderr.txt")
+
+        jobs.append((cmd_win, out_path, err_path, True))
+
+        # os.system(' '.join(cmd_win))
+        print("[*] IDA processing..")
+        with multiprocessing.Pool(processes=NUM_JOBS) as pool:
+            for _ in tqdm(
+                pool.imap_unordered(run_ida, jobs),
+                total=len(jobs),
+            ):
+                pass
+
+        # pool.apply_async(subprocess.call, args=(cmd,))
+    # pool.close()
+    # pool.join()
+
+    print("[*] Check..")
 
     # check whether generate .pkl is exists
     for bin_path in bin_pathes:
         bin_dir, bin_name = os.path.split(bin_path)
-        bin_dir_suffix = bin_dir[len(data_path):]
+        # bin_dir_suffix = bin_dir[len(DATA_ROOT):]
+        bin_dir_suffix = ""
         save_path = get_save_path(SAVE_ROOT, bin_dir_suffix, bin_name)
         if not os.path.exists(save_path):
             log_path = get_ida_log_path(ida_log_dir, bin_dir_suffix, bin_name)
-            print(f"[jTrans.run] XXXXXXXXXXXXXXX fail extract ida feat of {bin_path} into {save_path},\n"
-                  f"see log {log_path} for details.")
+            print(  f"[jTrans.run] XXXXXXXXXXXXXXX fail extract ida feat of {bin_path} into {save_path},\n"
+                    f"see log {log_path} for details.")
 
     print('[*] Features Extracting Done')
     # pairdata(SAVE_ROOT)
     end = time.time()
     print(f"[*] Time Cost: {end - start} seconds")
+
+if __name__ == '__main__':
+    main()
+
+# EOF
